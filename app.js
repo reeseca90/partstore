3cf127d45f436142cc0c4dc24f7fbe9d34dff18e
Craig <68542664+reeseca90@users.noreply.github.com>
2021-11-06 08:53:10 -0400

    Update app.js
    removed mongodb auth info

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://craig:libraryapp@cluster0.jqdmn.mongodb.net/partstore?retryWrites=true&w=majority';
+var mongoDB = 'mongodb+srv://<username>:<password>@cluster0.jqdmn.mongodb.net/partstore?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
